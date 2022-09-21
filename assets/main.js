4818a3baa0267d08c9894a7839f0ce1aa6ee2a8f
NoureddineMa <noureddinemaher13@gmail.com>
2022-09-22 01:02:37 +0200

    Add function refreshing when is DRAW

--- assets/main.js
+++ assets/main.js
@@ -3,10 +3,7 @@
         let title = document.getElementById('PlayerC');
         let squares = [];
         let turn = 'X';
-        // Get HTML elements:
 
-        let player_1 = document.getElementById('player1').value;
-        let player_2 = document.getElementById('player2').value;
         
         // Set data to localtstorage : 
         form.addEventListener("submit", function(e) {
@@ -19,9 +16,10 @@
         // Storing Data form:
         let p1 = localStorage.setItem('FirstPlayer',player_1);
         let p2 = localStorage.setItem('SecondPlayer',player_2); 
+        
         // Get Data :
         let GetFirstPlayer =  localStorage.getItem('FirstPlayer');
-        let GetSecondPlayer = localStorage.getItem('SecondPlayer');
+        let GetSecondPlayer = localStorage.getItem('SecondPlayer');        
         // Choose Player randomly : 
         let RandomPlay = [GetFirstPlayer,GetSecondPlayer];
         let resultPlayer = RandomPlay[Math.floor(Math.random()*RandomPlay.length)];
@@ -46,7 +44,7 @@
         else if (turn === 'O' && element.innerHTML == '' )
         {
         element.innerHTML = 'O';
-        element.style.color =  'red';
+        element.style.color =  'white';
         turn = 'X'
         title.innerHTML =  ' Turn : X';
         }
@@ -85,21 +83,22 @@
             else if(squares[3] == squares[5] && squares[5] == squares[7] && squares[5] != ''){
                 winner8();
         }
+        Draw();
         }
         // function coloring Winning Squares:
         function Result(num1,num2,num3){
             // take elements HTML: 
-            document.getElementById('item'+num1).style.background = '#000';
-            document.getElementById('item'+num2).style.background = '#000';
-            document.getElementById('item'+num3).style.background = '#000';
+            document.getElementById('item'+num1).style.background = '#25316D';
+            document.getElementById('item'+num2).style.background = '#25316D';
+            document.getElementById('item'+num3).style.background = '#25316D';
         }
         // Row 1,2,3 :who win?(X OR O):
         function winner1(){
             if(squares[1] == 'X' && squares[2] == 'X' && squares[3] == 'X'){
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> X Win The Game </h1>' 
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> X Win The Game </h1>' 
                 title.innerText = "X O GAME"
             } else if (squares[1] == 'O' && squares[2] == 'O' && squares[3] == 'O') {
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> O Win The Game </h1>';
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> O Win The Game </h1>';
                 title.innerText = "X O GAME"
             }
             Result(1,2,3);
@@ -108,10 +107,10 @@
         // Row 4,5,6:
         function winner2(){
             if(squares[4] == 'X' && squares[5] == 'X' && squares[6] == 'X'){
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> X Win The Game </h1>' 
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> X Win The Game </h1>' 
                 title.innerText = "X O GAME"
             } else if (squares[4] == 'O' && squares[5] == 'O' && squares[6] == 'O') {
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> O Win The Game </h1>';
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> O Win The Game </h1>';
                 title.innerText = "X O GAME"
             }
             Result(4,5,6);
@@ -120,10 +119,10 @@
         // Row 7,8,9:
         function winner3(){
             if(squares[7] == 'X' && squares[8] == 'X' && squares[9] == 'X'){
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> X Win The Game </h1>' 
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> X Win The Game </h1>' 
                 title.innerText = "X O GAME"
             }  else if (squares[7] == 'O' && squares[8] == 'O' && squares[9] == 'O') {
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> O Win The Game </h1>';
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> O Win The Game </h1>';
                 title.innerText = "X O GAME"
             }
             Result(7,8,9);
@@ -132,10 +131,10 @@
         // row 1,4,7:
         function winner4(){
             if(squares[1] == 'X' && squares[4] == 'X' && squares[7] == 'X'){
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> X Win The Game </h1>' 
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> X Win The Game </h1>' 
                 title.innerText = "X O GAME"
             }  else if (squares[1] == 'O' && squares[4] == 'O' && squares[7] == 'O') {
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> O Win The Game </h1>';
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> O Win The Game </h1>';
                 title.innerText = "X O GAME"
             }
             Result(1,4,7);
@@ -144,10 +143,10 @@
         // row 2,5,8:
         function winner5(){
             if(squares[2] == 'X' && squares[5] == 'X' && squares[8] == 'X'){
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> X Win The Game </h1>' 
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> X Win The Game </h1>' 
                 title.innerText = "X O GAME"
             }  else if (squares[2] == 'O' && squares[5] == 'O' && squares[8] == 'O') {
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> O Win The Game </h1>';
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> O Win The Game </h1>';
                 title.innerText = "X O GAME"
             }
             Result(2,5,8);
@@ -156,10 +155,10 @@
         // row 3,6,9:
         function winner6(){
             if(squares[3] == 'X' && squares[6] == 'X' && squares[9] == 'X'){
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> X Win The Game </h1>' 
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> X Win The Game </h1>' 
                 title.innerText = "X O GAME"
             } else if (squares[3] == 'O' && squares[6] == 'O' && squares[9] == 'O') {
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> O Win The Game </h1>' 
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> O Win The Game </h1>' 
                 title.innerText = "X O GAME"
             }
             Result(3,6,9);
@@ -168,10 +167,10 @@
         // row 1,5,9:
         function winner7(){
             if(squares[1] == 'X' && squares[5] == 'X' && squares[9] == 'X'){
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> X Win The Game </h1>' 
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> X Win The Game </h1>' 
                 title.innerText = "X O GAME"
             }  else if (squares[1] == 'O' && squares[5] == 'O' && squares[9] == 'O') {
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> O Win The Game </h1>';
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> O Win The Game </h1>';
                 title.innerText = "X O GAME"
             }
             Result(1,5,9);
@@ -180,10 +179,10 @@
 
         function winner8(){
             if(squares[3] == 'X' && squares[5] == 'X' && squares[7] == 'X'){
-                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> X Win The Game </h1>' 
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> X Win The Game </h1>' 
                 title.innerText = "X O GAME"
             }  else if (squares[3] == 'O' && squares[5] == 'O' && squares[7] == 'O') {
-                DeclareWinner.innerText = '<h1 style="padding: 20px 16px;background-color:black";padding:5px 10px;"> O Win The Game </h1>';
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> O Win The Game </h1>';
                 title.innerText = "X O GAME"
             }
             Result(3,5,7);
@@ -203,6 +202,16 @@
         let refresh = document.getElementById('refresh');
         refresh.innerHTML = '<input value="Refresh" type="submit" id="refresh" class="refresh" onclick="window.location.reload();">'
         }
+
+        // In case Draw:
+        function Draw(){
+            if(squares.every(e => e != "")){
+                if(squares.value != 'X' || 'O' ){
+                DeclareWinner.innerHTML = '<h1 style="padding: 20px 16px;background-color:#D6CDA4";padding:5px 10px;"> DRAW </h1>';
+                refresh();
+                }
+            }
+        } 
 
 
 
