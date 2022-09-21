23ee62046c31292b0c0ee9dba2f015d2c240b927
NoureddineMa <noureddinemaher13@gmail.com>
2022-09-21 13:40:53 +0200

    complete APP whithout declare 'name' of winner

--- assets/main.js
+++ assets/main.js
@@ -3,8 +3,7 @@
         let title = document.getElementById('PlayerC');
         let squares = [];
         let turn = 'X';
-
-        // get data to localtstorage : 
+        // Set data to localtstorage : 
         form.addEventListener("submit", function(e) {
         e.preventDefault();
         // Get HTML elements:
@@ -16,11 +15,9 @@
         // Get Data :
         let GetFirstPlayer =  localStorage.getItem('FirstPlayer');
         let GetSecondPlayer = localStorage.getItem('SecondPlayer');
-
         // Choose Player randomly : 
         let RandomPlay = [GetFirstPlayer,GetSecondPlayer];
         resultPlayer = RandomPlay[Math.floor(Math.random()*RandomPlay.length)];
-
         // Who play first: 
         if( GetFirstPlayer == '' && GetSecondPlayer == '' )
         {
@@ -29,65 +26,166 @@
             title.innerText =  resultPlayer.toUpperCase() + "  PLAY FIRST" ;
         }
         })
-    
         //   get id Square:
         function GetIdSquare(id){
 
-            let element = document.getElementById(id)
-            if(turn === 'X' && element.innerHTML == '' )
-            {
-                element.innerHTML = 'X';
-                turn = 'O';
-                title.innerHTML = 'O';
-            }
-            else if (turn === 'O' && element.innerHTML == '' )
-            {
-                element.innerHTML = 'O';
-                element.style.color =  'red';
-                turn = 'X'
-                title.innerHTML = 'X';
-            }
-            WhoWin();
+        let element = document.getElementById(id)
+        if(turn === 'X' && element.innerHTML == '' )
+        {
+        element.innerHTML = 'X';
+        turn = 'O';
+        title.innerHTML =  ' Turn : O';
         }
-
+        else if (turn === 'O' && element.innerHTML == '' )
+        {
+        element.innerHTML = 'O';
+        element.style.color =  'red';
+        turn = 'X'
+        title.innerHTML =  ' Turn : X';
+        }
+        WhoWin();
+        }
         // check Who Win: 
-            function WhoWin() {
-            for (let i= 1; i<10; i++)
+        // take element for HTML : 
+        let DeclareWinner = document.getElementById('winner');
+
+        function WhoWin() {
+        for (let i= 1; i<10; i++)
             {
                 squares[i] =  document.getElementById('item' + i).innerHTML;
             }
             if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != ''){
-                Result(1,2,3);
+                winner1();
             }
+
             else if(squares[4] == squares[5] && squares[5] == squares[6] && squares[5] != ''){
-                Result(4,5,6);
+                winner2();
             }
+
             else if(squares[7] == squares[8] && squares[8] == squares[9] && squares[8] != ''){
-                Result(7,8,9);
+                winner3();
             }
+
             else if(squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != ''){
-                Result(1,4,7);
+                winner4();
             }
+
             else if(squares[2] == squares[5] && squares[5] == squares[8] && squares[5] != ''){
-                Result(2,5,8);
+                winner5();
             }
+
             else if(squares[3] == squares[6] && squares[6] == squares[9] && squares[6] != ''){
-                Result(3,6,9);
+                winner6();
             }
+
             else if(squares[1] == squares[5] && squares[5] == squares[9] && squares[5] != ''){
-                Result(1,5,9);
+                winner7();
             }
+
             else if(squares[3] == squares[5] && squares[5] == squares[7] && squares[5] != ''){
-                Result(3,5,7);
-            }
+                winner8();
         }
-        
+        }
+
         function Result(num1,num2,num3){
+            // take elements HTML: 
             document.getElementById('item'+num1).style.background = '#000';
             document.getElementById('item'+num2).style.background = '#000';
             document.getElementById('item'+num3).style.background = '#000';
         }
+        // Row 1,2,3 :who win?(X OR O):
+        function winner1(){
+            if(squares[1] == 'X' && squares[2] == 'X' && squares[3] == 'X'){
+                DeclareWinner.innerText = "X who win the game "
+                title.innerText = "X O GAME"
+            } else if (squares[1] == 'O' && squares[2] == 'O' && squares[3] == 'O') {
+                DeclareWinner.innerText = "O who win the game ";
+                title.innerText = "X O GAME"
+            }
+            Result(1,2,3);
+        }
+        // Row 4,5,6:
+        function winner2(){
+            if(squares[4] == 'X' && squares[5] == 'X' && squares[6] == 'X'){
+                DeclareWinner.innerText = "X who win the game "
+                title.innerText = "X O GAME"
+            } else if (squares[4] == 'O' && squares[5] == 'O' && squares[6] == 'O') {
+                DeclareWinner.innerText = "O who win the game ";
+                title.innerText = "X O GAME"
+            }
+            Result(4,5,6);
+        }
+        // Row 7,8,9:
+        function winner3(){
+            if(squares[7] == 'X' && squares[8] == 'X' && squares[9] == 'X'){
+                DeclareWinner.innerText = "X who win the game "
+                title.innerText = "X O GAME"
+            }  else if (squares[7] == 'O' && squares[8] == 'O' && squares[9] == 'O') {
+                DeclareWinner.innerText = "O who win the game ";
+                title.innerText = "X O GAME"
+            }
+            Result(7,8,9);
+        }
+        // row 1,4,7:
+        function winner4(){
+            if(squares[1] == 'X' && squares[4] == 'X' && squares[7] == 'X'){
+                DeclareWinner.innerText = "X who win the game "
+                title.innerText = "X O GAME"
+            }  else if (squares[1] == 'O' && squares[4] == 'O' && squares[7] == 'O') {
+                DeclareWinner.innerText = "O who win the game ";
+                title.innerText = "X O GAME"
+            }
+            Result(1,4,7);
+        }
+        // row 2,5,8:
+        function winner5(){
+            if(squares[2] == 'X' && squares[5] == 'X' && squares[8] == 'X'){
+                DeclareWinner.innerText = "X who win the game "
+                title.innerText = "X O GAME"
+            }  else if (squares[2] == 'O' && squares[5] == 'O' && squares[8] == 'O') {
+                DeclareWinner.innerText = "O who win the game ";
+                title.innerText = "X O GAME"
+            }
+            Result(2,5,8);
+        }
+        // row 3,6,9:
+        function winner6(){
+            if(squares[3] == 'X' && squares[6] == 'X' && squares[9] == 'X'){
+                DeclareWinner.innerText = "X who win the game "
+                title.innerText = "X O GAME"
+            } else if (squares[3] == 'O' && squares[6] == 'O' && squares[9] == 'O') {
+                DeclareWinner.innerText = "O who win the game ";
+                title.innerText = "X O GAME"
+            }
+            Result(3,6,9);
+        }
+        // row 1,5,9:
+        function winner7(){
+            if(squares[1] == 'X' && squares[5] == 'X' && squares[9] == 'X'){
+                DeclareWinner.innerText = "X who win the game "
+                title.innerText = "X O GAME"
+            }  else if (squares[1] == 'O' && squares[5] == 'O' && squares[9] == 'O') {
+                DeclareWinner.innerText = "O who win the game ";
+                title.innerText = "X O GAME"
+            }
+            Result(1,5,9);
+        }
 
+        function winner8(){
+            if(squares[3] == 'X' && squares[5] == 'X' && squares[7] == 'X'){
+                DeclareWinner.innerText = "X who win the game "
+                title.innerText = "X O GAME"
+            }  else if (squares[3] == 'O' && squares[5] == 'O' && squares[7] == 'O') {
+                DeclareWinner.innerText = "O who win the game ";
+                title.innerText = "X O GAME"
+            }
+            Result(3,5,7);
+        }
+
+       
+       
+
+        
 
 
         
